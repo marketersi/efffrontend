d19d7422d51889fc67828b59d3ca7b39ec495a3c
Ayushi Pareek <ayushi563@gmail.com>
2025-02-21 11:03:22 +0530

    Mobile-responsive-changes

--- src/components/organisms/ux/UxSlider/Slider.tsx
+++ src/components/organisms/ux/UxSlider/Slider.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import "./slider.css";
 import { useSelector } from "react-redux";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const CoverFlowSlider = () => {
   const carousel = React.useRef(null);
@@ -11,11 +12,18 @@ const CoverFlowSlider = () => {
 
   const [slideIndex, setSlideIndex] = useState(0);
 
+  const NextArrow = ({ onClick }) => {
+    return <FaArrowRight className="custom-arrow next" onClick={onClick} />;
+  };
+  
+  const PrevArrow = ({ onClick }) => {
+    return <FaArrowLeft className="custom-arrow prev" onClick={onClick} />;
+  };
+
   
   const settings = {
     className: "center",
     centerMode: true,
- 
     infinite: true,
     centerPadding: "0",
     slidesToShow: 3,
@@ -25,18 +33,20 @@ const CoverFlowSlider = () => {
     dots: false,
     autoplaySpeed: 4000,
     cssEase: "linear",
+    arrows: true, // Enable arrows
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     beforeChange: (current, next) => setSlideIndex(next),
     responsive: [
       {
-        breakpoint: 1024, 
+        breakpoint: 1024,
         settings: {
           slidesToShow: 1,
-          
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 600, 
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -44,23 +54,21 @@ const CoverFlowSlider = () => {
       },
     ],
   };
-  
 
   return (
     <div className="slider-container">
       
       <Slider {...settings} className="UxSlider" ref={carousel}>
-        {slider?.images?.map((e, index) => {
-          return (
-            <div
-              key={index}
-              className={index === slideIndex ? "slide slide-active" : "slide"}
-            >
-              <a href={e.image_link} target="blank"><img src={e.Image} alt={`image ${index + 1}`} /></a>
-            </div>
-          );
-        })}
-      </Slider>
+  {slider?.images?.map((e, index) => {
+    return (
+      <div key={index} className={index === slideIndex ? "slide slide-active" : "slide"}>
+        <a href={e.image_link} target="blank">
+          <img src={e.Image} alt={`image ${index + 1}`} />
+        </a>
+      </div>
+    );
+  })}
+</Slider>;
     </div>
   );
 };
